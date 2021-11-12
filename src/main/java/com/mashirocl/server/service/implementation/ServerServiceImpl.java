08f38537e65b5eccfff066fd0ab82519e6d38158
MashiroCl <441016116@qq.com>
2021-11-12 23:41:05 +0900

    front end added

--- src/main/java/com/mashirocl/server/service/implementation/ServerServiceImpl.java
+++ src/main/java/com/mashirocl/server/service/implementation/ServerServiceImpl.java
@@ -12,6 +12,8 @@ import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.io.IOException;
 import java.net.InetAddress;
+import java.net.InetSocketAddress;
+import java.net.Socket;
 import java.util.Collection;
 import java.util.Random;
 
@@ -75,8 +77,19 @@ public class ServerServiceImpl implements ServerService {
     }
 
 
-    private String setServerImageUrl(){
-        String[] imageNames = {"server1.png","server2.png","server3.png","server4.png"};
-        return ServletUriComponentsBuilder.fromCurrentContextPath().path("/server/image"+imageNames[new Random().nextInt()]).toUriString();
+    private String setServerImageUrl() {
+        String[] imageNames = { "server1.png", "server2.png", "server3.png", "server4.png" };
+        return ServletUriComponentsBuilder.fromCurrentContextPath().path("/server/image/" + imageNames[new Random().nextInt(4)]).toUriString();
+    }
+
+    private boolean isReachable(String ipAddress, int port, int timeOut) {
+        try {
+            try(Socket socket = new Socket()) {
+                socket.connect(new InetSocketAddress(ipAddress, port), timeOut);
+            }
+            return true;
+        }catch (IOException exception){
+            return false;
+        }
     }
 }
